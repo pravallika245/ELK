e3cca117f8def7ad768688bbde6aca05e5e74be5
pravallika245 <pravallika45@gmail.com>
2020-01-11 22:43:16 +0530

    khlk

---
+++ Sample/src/test/java/com/test/SampleTest.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class SampleTest {
+
+	@Test
+	public void validatejunit() {
+		Assert.assertTrue(true);
+	}
+}
